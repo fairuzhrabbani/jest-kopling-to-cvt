e8f7f95f07806ec0f1d7db9c03b76235de3b160f
Fairuz Hanif Rabbani <fairuzhrabbani@gmail.com>
2026-09-03 21:33:05 +0700

    feat: add file login_fourth

--- tests/auth/login_fourth.test.js
+++ tests/auth/login_fourth.test.js
@@ -1,12 +1,21 @@
 import authAPI from '../../api/AuthAPI.js';
 import { loginTestData } from '../../data/auth/login.data.js';
+import {
+  expectSuccessResponse,
+  expectErrorResponse,
+} from '../../utils/assertions/common.assert.js';
+
+import { expectLoginSuccess } from '../../utils/assertions/auth.assert.js';
 
 // describe the test suite for the Login API
 describe('Auth API', () => {
   describe('Positive Scenarios', () => {
     test('LOGIN-001 : Return 200 when Valid Credentials', async () => {
+      // request the login API using the AuthAPI class and the provided credentials
       const response = await authAPI.login(loginTestData.validCredentials);
 
+      // Assertions to verify the response from the login API
+      // expect the response status to be 200 (OK) and the response body to contain the expected properties
       expectSuccessResponse(response, {
         status: 200,
         message: 'Login successful',
@@ -30,6 +39,7 @@ describe('Auth API', () => {
         status: 401,
         message: 'Invalid email or password',
       });
+      // expectLoginError(response, 401, 'Invalid email or password');
     });
 
     test('LOGIN-003 : Return 401 when Email Not Found', async () => {
@@ -46,37 +56,46 @@ describe('Auth API', () => {
     test('LOGIN-004 : Return 400 when Email is Empty', async () => {
       const response = await authAPI.login(loginTestData.emptyEmail);
 
-      expectErrorResponse(response, {
-        status: 400,
-        message: 'Email and password are required',
-      });
+      console.log(response.body);
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('success', false);
+      expect(response.body).toHaveProperty(
+        'message',
+        'Email and password are required',
+      );
     });
 
     test('LOGIN-005 : Return 400 when Password is Empty', async () => {
       const response = await authAPI.login(loginTestData.emptyPassword);
 
-      expectErrorResponse(response, {
-        status: 400,
-        message: 'Email and password are required',
-      });
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('success', false);
+      expect(response.body).toHaveProperty(
+        'message',
+        'Email and password are required',
+      );
     });
 
     test('LOGIN-006 : Return 401 when Email is Invalid Format', async () => {
       const response = await authAPI.login(loginTestData.invalidEmailFormat);
 
-      expectErrorResponse(response, {
-        status: 401,
-        message: 'Invalid email or password',
-      });
+      expect(response.status).toBe(401);
+      expect(response.body).toHaveProperty('success', false);
+      expect(response.body).toHaveProperty(
+        'message',
+        'Invalid email or password',
+      );
     });
 
     test.skip('LOGIN-007 : Return 401 when Email is Invalid Format', async () => {
       const response = await authAPI.login(loginTestData.emailInvalidFormat);
 
-      expectErrorResponse(response, {
-        status: 401,
-        message: 'Invalid email or password',
-      });
+      expect(response.status).toBe(401);
+      expect(response.body).toHaveProperty('success', false);
+      expect(response.body).toHaveProperty(
+        'message',
+        'Invalid email or password',
+      );
     });
   });
 });
